4482615d2753f6931188d19b8f631d57356ad0a3
Veronika <veronika.pilipenko@gmail.com>
2021-11-02 20:51:28 -0400

    state declared and jsx skeleton complete

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,14 +1,27 @@
 import React from "react";
 
 class ToDo extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    // }
+    constructor(props) {
+        super(props);
+        this.state= {
+            newTask: "",
+            list: [],
+        }
+    }
 
     render() {
         return (
             <div>
-                <h1>hello this is going to be the to do component</h1>
+                <h1>Add an Item to your to to list...</h1>
+                    <br/>
+                    <input type="text"
+                    placeholder="Type an item here..."
+                    value={this.state.newTask}
+                    onChange={e => this.updateInput("newTask, e.target.value")}
+                    />
+
+                <button onClick={() => this.addItem()}> add
+                </button>     
             </div>
         )
     }
